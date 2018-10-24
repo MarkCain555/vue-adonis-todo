c3bdaa49ea4469fba268eefea7a2c6115ce61728
Cain <cainml1@leidos.com>
2018-10-24 12:09:17 -0700

    editable records and create records components

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,38 +1,25 @@
 <template>
     <Panel title="Projects">
         <div class="project mt-2" v-for="project in projects" :key="project.id">
-            <v-layout row wrap>
-                <v-flex xs9 text-xs-left>
-                    <span v-if="!project.isEditMode">
-                        {{project.title}}
-                    </span>
-                    <v-text-field autofocus @keyup.enter="saveProject(project)" v-if="project.isEditMode" :value="project.title" @input="setProjectTitle({ project, title: $event})"></v-text-field>
-                </v-flex>
-                <v-flex xs3 text-xs-right>
-                    <v-icon v-if="!project.isEditMode" @click="setEditMode(project)">edit</v-icon>
-                    <v-icon v-if="project.isEditMode" @click="saveProject(project)">check</v-icon>
-                    <v-icon @click="deleteProject(project)">delete</v-icon>
-                </v-flex>
-            </v-layout>
+            <EditableRecord :isEditMode="project.isEditMode" :title="project.title" @onInput="setProjectTitle({
+                    project,
+                    title: $event,
+                })" @onEdit="setEditMode(project)" @onSave="saveProject(project)" @onDelete="deleteProject(project)" />
         </div>
-        <v-layout row wrap class="mt-4">
-            <v-flex xs8>
-                <v-text-field placeholder="My project name..." @keyup.enter="createProject" :value="newProjectTitle" @input="setNewProjectTitle"></v-text-field>
-            </v-flex>
-            <v-flex xs4>
-                <v-btn small dark class="mt-4" color="green" @click="createProject">
-                    <v-icon class="mr-2">add_circle</v-icon>
-                    Create
-                </v-btn>
-            </v-flex>
-        </v-layout>
+        <CreateRecord placeholder="My project name..." @onInput="setNewProjectTitle" :value="newProjectTitle" @create="createProject" />
     </Panel>
 </template>
 
 <script>
 import Panel from './Panel';
+import CreateRecord from './CreateRecord';
+import EditableRecord from './EditableRecord';
 import { mapState, mapMutations, mapActions } from 'vuex';
 export default {
+	components: {
+		CreateRecord,
+		EditableRecord,
+	},
 	mounted() {
 		this.fetchProjects();
 	},
